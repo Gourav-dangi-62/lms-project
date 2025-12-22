a61a7304f9b296107a70b80f0a9a7aee7d35ff43
Gourav-dangi-62 <dangigourav75@gmail.com>
2025-12-22 18:55:53 +0530

    Complete Homepage layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 
 import { Route, Routes } from 'react-router-dom'
 import Footer from './Components/Footer.jsx'
+import HomePage from './Pages/HomePage.jsx'
 import './App.css'
+import HomeLayout from './Layouts/HomeLayout.jsx'
 
 function App() {
 
   return (
-    <> 
-    {/* <Routes> */}
-  {/* <Route path='/' element={<Home />} ></Route>/> */}
-    {/* </Routes> */}
-    {/* <Footer /> */}
-       </>
+    <>
+      <Routes>
+
+        <Route path="/" element={<HomePage />} ></Route>
+      </Routes>
+
+    </>
   )
 }
 
